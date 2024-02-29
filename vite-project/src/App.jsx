566835f693a9b0c80750a981e87146e58b3e3571
nesrine ghanem <ghnessrine92@gmail.com>
2024-02-29 22:30:49 +0100

    Break App Into Components

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,79 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css"
+import { useEffect, useState } from "react"
+import { NewTodoForm } from "./NewTodoForm"
+import { TodoList } from "./TodoList"
 
-function App() {
+export default function App() {
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null) return []
 
-  const [newitem ,setNewItem] = useState("")
-  const [todos,setTodos] = useState([])
-  function handleSubmit(e){
-    e.preventDefault()
+    return JSON.parse(localValue)
+  })
 
-    setTodos((currentTodos)=>{
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos])
+
+  function addTodo(title) {
+    setTodos(currentTodos => {
       return [
         ...currentTodos,
-      {id:crypto.randomUUID(), title: newitem , completed:false},
+        { id: crypto.randomUUID(), title, completed: false },
       ]
     })
-    setNewItem('')
-   }
+  }
 
-   function toggleTodo (id,completed){
-    setTodos(currentTodos=>{
-      return(
-      currentTodos.map(todos=>{
-        if(todos.id === id){
-          return{...todos,completed}
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed }
         }
-        return todos
+
+        return todo
       })
-      )
     })
-   }
-   function deleteTodo (id) {
-    setTodos(currentTodos =>{
-      return (
-        currentTodos.filter(todos.id !== id)
-      )
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
     })
-   }
+  }
+
   return (
     <>
-    <form className='new-item-form' onSubmit={handleSubmit}>
-      <div className='forme-row'>
-        <label htmlFor='item'>New item</label>
-        <input type="text" id="item" 
-        value={newitem}
-        onChange={e=>setNewItem(e.target.value)}
-        />
-      </div>
-      </form>
-      <h1 className='header'>Todo List</h1>
-      <ul className='list'>
-        {todos.length === 0 && "No Todos"}
-        {todos.map(todos=>{
-          return(
-          <li key={todos.id}>
-        <label>
-          <input type='checkbox' checked={todos.completed}
-          onChange={e=> toggleTodo(todos.id , e.target.checked)}/>
-          {todos.title}
-          </label>
-          <button className='btn btn-danger'
-           onClick={()=> deleteTodo(todos.id)}
-          >
-            Delete</button>
-       </li>
-       )
-        })}
-      
-      </ul>
-
-   
-    
+      <NewTodoForm onSubmit={addTodo} />
+      <h1 className="header">Todo List</h1>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
     </>
   )
 }
-
-export default App
